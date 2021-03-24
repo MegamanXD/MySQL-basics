5e75d85fabda173b6ae9f3d4da37170ffcb638f0
MegamanXD <s3759797@rmit.edu.vn>
2021-03-24 14:55:08 +0700

    Added more detailed comments

--- 1_database_level.sql
+++ 1_database_level.sql
@@ -1,3 +1,4 @@
+# Full tutorial: https://www.sqlshack.com/learn-sql-create-database-create-table/
 # Create
 create database if not exists test;
 use test;
--- 4_advanced_commands.sql
+++ 4_advanced_commands.sql
@@ -12,20 +12,25 @@ WHERE date = '2014-08-25'
 AND tests.test_id = scores.test_id
 AND scores.student_id = students.student_id;
 
-# Left join
-# Display all info from 1 table, even if the other table doesn’t have a match)
-SELECT students.student_id,
-CONCAT(students.first_name, " ", students.last_name) AS Name,
-COUNT(absences.date) AS Absences
-FROM students LEFT JOIN absences
-ON students.student_id = absences.student_id
-GROUP BY students.student_id;
-
 # Inner join
-# Pull all data from both tables and display them
+# INNER JOIN vs NATURAL JOIN: https://www.geeksforgeeks.org/difference-between-natural-join-and-inner-join-in-sql/
+# Show all data from all tables, even if the other table doesn't have a match
+# Data without a match will be displayed as NULL
 SELECT students.first_name, students.last_name, scores.test_id, scores.score
 FROM students INNER JOIN scores
 ON students.student_id=scores.student_id
 WHERE scores.score <= 15
 ORDER BY scores.test_id;
+
+# Left join
+# LEFT JOIN vs INNER JOIN: https://www.sqlshack.com/learn-sql-inner-join-vs-left-join/
+# Display all info from 1 table, even if the other table doesn’t have a match
+# Data without a match will be displayed as NULL
+
+SELECT students.student_id,
+CONCAT(students.first_name, " ", students.last_name) AS Name,
+COUNT(absences.date) AS Absences
+FROM students LEFT JOIN absences    # Display all students, even if they have no absence
+ON students.student_id = absences.student_id
+GROUP BY students.student_id;
 
